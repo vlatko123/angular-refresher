624357e3cc24b4c67ffb634744a6cba749131f61
vlatko123 <vlatkobogojevski@yahoo.com>
2025-12-15 00:10:05 +0100

    add task app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+  
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId);
+  }
+}
---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../../dummy-tasks.service';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CardComponent, DatePipe],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({required: true}) task!: Task;
+  
+  constructor(private tasksService: TasksService) {}
+
+  onCompleteTask() {
+    this.tasksService.removeTask(this.task.id);
+  }
+
+}
